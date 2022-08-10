156ab8cb878ff6e89279f2c722e0aa524ea38f69
[TAREQUE-AHMED] <[tarequeahmed46@gmail.com]>
2022-08-11 00:36:53 +0600

    pattern-2

---
+++ loop/pattern-2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int col, row, n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    for ( row = 1; row<=n; row++)
+    {
+        for ( col = 1; col<=row; col++)
+        {
+            printf("%d ", row);
+        }
+        printf("\n");
+    }
+    return 0;
+}
